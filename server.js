7b56dcc9eb195060d40c8a86d2b8eb05ce86a24b
JayW24 <witkowski93kuba@gmail.com>
2022-10-03 01:56:44 +0200

    heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const http = require('http');
 const app = express();
 const server = http.createServer(app);
 const port = process.env.PORT || 5000;
-
+const colors = require('./colorfulLogs').colors;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 //TESTS START

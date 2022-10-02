a47ea153df0e3423313c823f29de2a357e4912f8
JayW24 <witkowski93kuba@gmail.com>
2022-10-03 01:24:57 +0200

    heroku

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-
+const colors = require('./colorfulLogs').colors;
 const app = express();
 const port = process.env.PORT || 5000;
+const http = require('http');
+const server = http.createServer(app);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -30,4 +32,11 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+server.listen(port, () => {
+	console.log(colors.FgCyan, '\n*\n*\n*\n*\n*\n*\n')
+	console.log(colors.FgYellow, '[Database]: database connected! Mongo Client and Mongoose are ready.')
+	console.log(colors.FgBlue, `[Server]: server started. Listening on port *:${port}`)
+}).on('error', (err) => {
+	console.log(colors.FgRed, '[Server]: Server listen error!')
+	console.log(err)
+})

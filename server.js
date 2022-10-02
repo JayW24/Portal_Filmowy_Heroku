b2fd24aa2b07bce547746ff0ec43fe9c6f9f5e10
JayW24 <witkowski93kuba@gmail.com>
2022-10-02 05:37:02 +0200

    hroku

--- server.js
+++ server.js
@@ -97,6 +97,17 @@ passport.deserializeUser(function (id, done) {
 	})
 });
 
+//
+if (process.env.NODE_ENV === 'production') {
+	// Serve any static files
+	app.use(express.static(path.join(__dirname, 'client/build')));
+  
+	// Handle React routing, return all requests to React app
+	app.get('*', function(req, res) {
+	  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+	});
+  }
+//
 
 // ************************************************* START SERVER + DATABASE CONNECTIONS ***********************************************
 // Start Mongoose 

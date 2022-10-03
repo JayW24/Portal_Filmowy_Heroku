359d52fabe18da1f803057aef60f60a072ac4b9f
JayW24 <witkowski93kuba@gmail.com>
2022-10-03 02:23:57 +0200

    heroku

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 //added stuff
 const connectionOptions = { poolSize: process.env.MONGO_POOLSIZE || 1 }
 const mongodb = require('mongodb')

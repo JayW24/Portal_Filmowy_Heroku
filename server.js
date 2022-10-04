45d4c2b2fce7e2d319497e9a3965f7ef49bc650d
JayW24 <witkowski93kuba@gmail.com>
2022-10-04 21:48:28 +0200

    heroku messenger test

--- server.js
+++ server.js
@@ -1529,15 +1529,15 @@ function serverLog(req, method) {
 }
 // END OF ALL GUWNO TEST
 
-// if (process.env.NODE_ENV === 'production') {
-//   // Serve any static files
-//   app.use(express.static(path.join(__dirname, 'client/build')));
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
 
-//   // Handle React routing, return all requests to React app
-//   app.get('*', function(req, res) {
-//     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-//   });
-// }
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 // ************************************************* START SERVER + DATABASE CONNECTIONS ***********************************************
 // Start Mongoose 

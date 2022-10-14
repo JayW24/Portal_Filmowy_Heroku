fe04ea333695ce7b3512d188ec1d803c8277b3af
JayW24 <witkowski93kuba@gmail.com>
2022-10-14 13:39:04 +0200

    fullscreen search styling

--- client/src/components/SearchFullScreen.js
+++ client/src/components/SearchFullScreen.js
@@ -7,14 +7,14 @@ export default function SearchFullScreen(props) {
     <div className="full-screen-search">
       <div>
         <button className="close-button search-button" onClick={props.onClose}>X</button>
-        <label>Szukaj...</label>
+        <h6 className="fullScreenSearchHeader">Szukaj</h6>
         <input id="fullScreenSearchInput" className="full-screen-search-input" type="text" autoComplete="off" name="searchlarge" onChange={props.onChange} />
-        Wyniki:
+        <h6 className="fullScreenSearchHeader">Wyniki:</h6>
         <div id="full-screen-search-results" onClick={props.onClose}>
           {props.searchResult.map(searchResult => {
             return (
               <Link to={`/${getDbNameByType(searchResult.type)}/${searchResult._id}`}>
-                <div className="d-flex align-items-center p-0 m-0 mb-1 search-item">
+                <div className="d-flex align-items-center p-1 m-0 mb-1 search-item">
                   <div className="col-1 d-flex justify-content-center p-1 search-item-img-container">
                     <img className="search-thumbnail" src={searchResult.thumbnail} alt={searchResult.thumbnail} />
                   </div>
